744d4f656f6d1f98643109d6c60f696b807594d2
shehroze-1122 <stalat.bee19seecs@seecs.edu.pk>
2025-01-01 16:45:49 +0100

    fix: replace named json import by default one

--- components/pages/home/Work/Work.tsx
+++ components/pages/home/Work/Work.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Job from "./Job";
-import { jobs } from "./data.json";
+import data from "./data.json";
 
 function Work() {
-  const chronologicalOrder = [...jobs].reverse();
+  const jobs = [...data.jobs].reverse();
   return (
     <section className="flex flex-col gap-5">
       <h2>Work</h2>
       <div className="flex flex-col gap-3">
-        {chronologicalOrder.map((job) => (
+        {jobs.map((job) => (
           <Job
             key={job.id}
             company={job.company}
