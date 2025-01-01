571ff31a9f6b2f0d1e33103c537b8164128d6703
shehroze-1122 <stalat.bee19seecs@seecs.edu.pk>
2025-01-01 17:36:31 +0100

    chore: change span to div in text slider

--- components/layout/Header/Avatar/TitleSlider/TitleSlider.tsx
+++ components/layout/Header/Avatar/TitleSlider/TitleSlider.tsx
@@ -8,18 +8,18 @@ type TextSliderProps = {
 
 function TextSlider({ className }: TextSliderProps) {
   const classNames = twMerge(
-    clsx("block relative h-[1em] w-36 overflow-hidden", className)
+    clsx("relative h-[1em] w-36 overflow-hidden", className)
   );
 
   return (
-    <span className={classNames}>
+    <div className={classNames}>
       <span className="absolute h-full w-full -translate-y-full animate-slide leading-none">
         Product Engineer
       </span>
       <span className="absolute h-full w-full -translate-y-full animate-slide leading-none [animation-delay:2s]">
         Full Stack Engineer
       </span>
-    </span>
+    </div>
   );
 }
 
