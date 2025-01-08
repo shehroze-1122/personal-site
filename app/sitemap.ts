673e04fc9b230d05f5a65a35593d883c97cf501d
shehroze-1122 <stalat.bee19seecs@seecs.edu.pk>
2025-01-08 22:18:51 +0100

    update sitemap with blog links

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,11 +1,17 @@
 import config from "@/config";
+import { getAllPosts } from "@/lib/api/posts";
 
 export default async function sitemap() {
   const baseUrl = config.baseUrl;
-  const paths = ["", "blog"].map((path) => ({
+  const allPosts = getAllPosts();
+  const basePaths = ["", "blog"].map((path) => ({
     url: `${baseUrl}/${path}`,
     lastModified: new Date().toISOString(),
   }));
+  const postsPaths = allPosts.map((post) => ({
+    url: `${baseUrl}/blog/${post.slug}`,
+    lastModified: post.updatedAt,
+  }));
 
-  return paths;
+  return [...basePaths, ...postsPaths];
 }
