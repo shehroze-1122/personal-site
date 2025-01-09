37a98fd5f641419e69c057ad878e3bd6639c8acd
shehroze-1122 <stalat.bee19seecs@seecs.edu.pk>
2025-01-09 22:46:53 +0100

    chore: add aria labels to my footer links

--- components/layout/Footer/Links/Links.tsx
+++ components/layout/Footer/Links/Links.tsx
@@ -6,8 +6,13 @@ function Links() {
   const links = data.links;
   return (
     <div className="flex flex-row items-center gap-2">
-      {links.map(({ Icon, key, href }) => (
-        <IconLink key={key} href={href} target="_blank">
+      {links.map(({ Icon, key, href, type }) => (
+        <IconLink
+          key={key}
+          href={href}
+          target="_blank"
+          aria-label={`Link to my ${type}`}
+        >
           <Icon height={25} width={25} />
         </IconLink>
       ))}
